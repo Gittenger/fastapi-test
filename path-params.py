90ea7a97f9f37dfde6cd219dc12f4daa8351149c
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-06-22 03:11:36 -0400

    ModelName type

--- path-params.py
+++ path-params.py
@@ -1,3 +1,12 @@
+from enum import Enum
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
