0fa57d5bb7408af6922a26b7eb29fd0af3db4f02
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-06-23 00:19:36 -0400

    path params, numeric validations

---
+++ path-numeric-validations.py
@@ -0,0 +1,33 @@
+from enum import Enum
+
+from typing import Union
+from fastapi import FastAPI, Path, Query
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+# add metadata
+@app.get("/items/{item_id}")
+async def read_items(
+    item_id: int = Path(title="The ID of the item to get"),
+    q: str | None = Query(default=None, alias="item-query"),
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results
+
+
+# ls/eq gt/eq, floats, ints
+@app.get("/items/{item_id}")
+async def read_items(
+    *,
+    item_id: int = Path(title="The ID of the item to get", ge=0, le=1000),
+    q: str,
+    size: float = Query(gt=0, lt=10.5)
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results
